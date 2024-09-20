19e424894d8c8d1a9446ef1eb32b45a9050c2db2
Neil <neil@ranlay.com>
2024-09-20 16:23:09 -0400

    Implemented tree map visualization to show WSD performance for models and added dataset from WSD evaluation

---
+++ d_99_wsd_performance_tree_map.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import numpy as np
+
+df = pd.read_csv("tests/tree_data_q1.csv")
+
+fig = px.treemap(
+    df,
+    names=df["label"],
+    values=df["assessment count"],
+    ids=df["synset_id"],
+    parents=df["parent_synset"],
+    color="open-ai-gpt-4-0613",
+    color_continuous_scale="RdBu",
+)
+fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+fig.show()

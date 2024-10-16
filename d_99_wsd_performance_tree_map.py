2ab20e00971564aa9d37c1c6bb75716d9771e17c
Neil Ranly <neil.ranly@gmail.com>
2024-10-16 09:35:00 -0400

    Added diagram for process; improved legend of color-map

--- d_99_wsd_performance_tree_map.py
+++ d_99_wsd_performance_tree_map.py
@@ -4,13 +4,15 @@ import numpy as np
 
 df = pd.read_csv("tests/tree_data_q1.csv")
 
+df["Accuracy (%)"] = df["open-ai-gpt-4-0613"] * 100
+
 fig = px.treemap(
     df,
     names=df["label"],
     values=df["assessment count"],
     ids=df["synset_id"],
     parents=df["parent_synset"],
-    color="open-ai-gpt-4-0613",
+    color="Accuracy (%)",
     color_continuous_scale="RdBu",
 )
 fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
